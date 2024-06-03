8adc41d9b6576de4b323bcea4b22247f44425f7f
Abhay Singh Gill <abhaygill017@gmail.com>
2024-06-03 20:37:29 +0530

    ParanoidGlyph: Improve volume level animation
    - It runs a bit smoother than before now.
    
    Change-Id: I81ecbad8fee4cad847ac6407e4fc94fd54eeaa4f
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/com/pixys/glyph/Manager/StatusManager.java
+++ src/com/pixys/glyph/Manager/StatusManager.java
@@ -26,7 +26,6 @@ public final class StatusManager {
     private static boolean callLedActive = false;
     private static boolean essentialLedActive = false;
     private static boolean volumeLedActive = false;
-    private static int volumeLedLast = 0;
     private static boolean volumeLedUpdate = false;
 
     private static boolean callLedEnabled = false;
@@ -71,14 +70,6 @@ public final class StatusManager {
 
     public static void setVolumeLedActive(boolean status) {
         volumeLedActive = status;
-    }
-
-    public static int getVolumeLedLast() {
-        return volumeLedLast;
-    }
-
-    public static void setVolumeLedLast(int last) {
-        volumeLedLast = last;
     }
 
     public static boolean isVolumeLedUpdate() {
