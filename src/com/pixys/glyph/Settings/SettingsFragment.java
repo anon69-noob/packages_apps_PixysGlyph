AOSPA/android_packages_apps_ParanoidGlyph

ParanoidGlyph: Add music visualizer tile
Change-Id: I76c52e841bf7b42eb8822ffd901b91cd19fe80cd
Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

src/co/aospa/glyph/Settings/SettingsFragment.java
@@ -19,6 +19,8 @@
 package co.aospa.glyph.Settings;
 
 import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -30,6 +32,7 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SeekBarPreference;
 import androidx.preference.SwitchPreference;
 
@@ -43,7 +46,7 @@ import co.aospa.glyph.Manager.SettingsManager;
 import co.aospa.glyph.Utils.ServiceUtils;
 
 public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
-        OnMainSwitchChangeListener {
+        OnMainSwitchChangeListener, OnSharedPreferenceChangeListener {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -64,6 +67,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.glyph_settings);
+
+        PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT).registerOnSharedPreferenceChangeListener(this);
 
         mContentResolver = getActivity().getContentResolver();
         mSettingObserver = new SettingObserver();
@@ -180,7 +185,24 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     }
 
     @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE)) {
+            boolean isChecked = SettingsManager.isGlyphMusicVisualizerEnabled();
+            mMusicVisualizerPreference.setChecked(isChecked);
+            mFlipPreference.setEnabled(!isChecked);
+            mNotifsPreference.setEnabled(!isChecked);
+            mNotifsPreference.setSwitchEnabled(!isChecked);
+            mCallPreference.setEnabled(!isChecked);
+            mCallPreference.setSwitchEnabled(!isChecked);
+            mChargingLevelPreference.setEnabled(!isChecked);
+            mVolumeLevelPreference.setEnabled(!isChecked);
+            mChargingPowersharePreference.setEnabled(!isChecked);
+        }
+    }
+
+    @Override
     public void onDestroy() {
+        PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT).registerOnSharedPreferenceChangeListener(this);
         mSettingObserver.unregister(mContentResolver);
         super.onDestroy();
     }
@@ -191,6 +213,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         }
 
         public void register(ContentResolver cr) {
+            cr.registerContentObserver(Settings.Secure.getUriFor(
+                Constants.GLYPH_ENABLE), false, this);
             cr.registerContentObserver(Settings.Secure.getUriFor(
                 Constants.GLYPH_CALL_ENABLE), false, this);
             cr.registerContentObserver(Settings.Secure.getUriFor(
@@ -204,6 +228,9 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
+            if (uri.equals(Settings.Secure.getUriFor(Constants.GLYPH_ENABLE))) {
+                mSwitchBar.setChecked(SettingsManager.isGlyphEnabled());
+            }
             if (uri.equals(Settings.Secure.getUriFor(Constants.GLYPH_CALL_ENABLE))) {
                 mCallPreference.setChecked(SettingsManager.isGlyphCallEnabled());
             }

src/co/aospa/glyph/Tiles/MusicVisualizerTileService.java
@@ -1,0 +1,66 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2022-2024 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.glyph.Tiles;
+
+import android.content.SharedPreferences; 
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.preference.PreferenceManager;
+
+import co.aospa.glyph.Constants.Constants;
+import co.aospa.glyph.R;
+import co.aospa.glyph.Manager.SettingsManager;
+import co.aospa.glyph.Utils.ServiceUtils;
+
+/** Quick settings tile: Glyph **/
+public class MusicVisualizerTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = getEnabled();
+        getQsTile().setContentDescription(enabled ?
+                getString(R.string.glyph_accessibility_quick_settings_on) :
+                getString(R.string.glyph_accessibility_quick_settings_off));
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        setEnabled(!getEnabled());
+        updateState();
+    }
+
+    private boolean getEnabled() {
+        return SettingsManager.isGlyphMusicVisualizerEnabled();
+    }
+
+    private void setEnabled(boolean enabled) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT);
+        sharedPrefs.edit().putBoolean(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE, enabled).apply();
+        ServiceUtils.checkGlyphService();
+    }
+}

src/co/aospa/glyph/Tiles/TileActivity.java
@@ -39,7 +39,8 @@ public class TileActivity extends Activity {
         ComponentName sourceClass = getIntent().getParcelableExtra(Intent.EXTRA_COMPONENT_NAME);
         if (DEBUG) Log.d(TAG, "sourceClass: " + sourceClass.getClassName());
         if (sourceClass.getClassName().equals("co.aospa.glyph.Tiles.GlyphTileService")
-            || sourceClass.getClassName().equals("co.aospa.glyph.Tiles.TorchTileService")) {
+            || sourceClass.getClassName().equals("co.aospa.glyph.Tiles.TorchTileService")
+            || sourceClass.getClassName().equals("co.aospa.glyph.Tiles.MusicVisualizerTileService")) {
             openActivitySafely(new Intent(this, SettingsActivity.class));
         } else {
             finish();

anon69-noob/packages_apps_PixysGlyph

src/com/pixys/glyph/Settings/SettingsFragment.java
@@ -19,6 +19,8 @@
 package com.pixys.glyph.Settings;
 
 import android.content.ContentResolver;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,6 +34,7 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SeekBarPreference;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -44,7 +47,7 @@ import com.pixys.glyph.Manager.SettingsManager;
 import com.pixys.glyph.Utils.ServiceUtils;
 
 public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
-        OnCheckedChangeListener {
+        OnCheckedChangeListener, OnSharedPreferenceChangeListener {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -65,6 +68,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.glyph_settings);
+
+        PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT).registerOnSharedPreferenceChangeListener(this);
 
         mContentResolver = getActivity().getContentResolver();
         mSettingObserver = new SettingObserver();
@@ -181,7 +186,24 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     }
 
     @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE)) {
+            boolean isChecked = SettingsManager.isGlyphMusicVisualizerEnabled();
+            mMusicVisualizerPreference.setChecked(isChecked);
+            mFlipPreference.setEnabled(!isChecked);
+            mNotifsPreference.setEnabled(!isChecked);
+            mNotifsPreference.setSwitchEnabled(!isChecked);
+            mCallPreference.setEnabled(!isChecked);
+            mCallPreference.setSwitchEnabled(!isChecked);
+            mChargingLevelPreference.setEnabled(!isChecked);
+            mVolumeLevelPreference.setEnabled(!isChecked);
+            mChargingPowersharePreference.setEnabled(!isChecked);
+        }
+    }
+
+    @Override
     public void onDestroy() {
+        PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT).registerOnSharedPreferenceChangeListener(this);
         mSettingObserver.unregister(mContentResolver);
         super.onDestroy();
     }
@@ -192,6 +214,8 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         }
 
         public void register(ContentResolver cr) {
+            cr.registerContentObserver(Settings.Secure.getUriFor(
+                Constants.GLYPH_ENABLE), false, this);
             cr.registerContentObserver(Settings.Secure.getUriFor(
                 Constants.GLYPH_CALL_ENABLE), false, this);
             cr.registerContentObserver(Settings.Secure.getUriFor(
@@ -205,6 +229,9 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
+            if (uri.equals(Settings.Secure.getUriFor(Constants.GLYPH_ENABLE))) {
+                mSwitchBar.setChecked(SettingsManager.isGlyphEnabled());
+            }
             if (uri.equals(Settings.Secure.getUriFor(Constants.GLYPH_CALL_ENABLE))) {
                 mCallPreference.setChecked(SettingsManager.isGlyphCallEnabled());
             }

src/com/pixys/glyph/Tiles/MusicVisualizerTileService.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
+ * Copyright (C) 2022-2024 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixys.glyph.Tiles;
+
+import android.content.SharedPreferences; 
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+import androidx.preference.PreferenceManager;
+
+import com.pixys.glyph.Constants.Constants;
+import com.pixys.glyph.R;
+import com.pixys.glyph.Manager.SettingsManager;
+import com.pixys.glyph.Utils.ServiceUtils;
+
+/** Quick settings tile: Glyph **/
+public class MusicVisualizerTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        updateState();
+    }
+
+    private void updateState() {
+        boolean enabled = getEnabled();
+        getQsTile().setContentDescription(enabled ?
+                getString(R.string.glyph_accessibility_quick_settings_on) :
+                getString(R.string.glyph_accessibility_quick_settings_off));
+        getQsTile().setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        getQsTile().updateTile();
+    }
+
+    @Override
+    public void onClick() {
+        super.onClick();
+        if (SettingsManager.isGlyphEnabled()) {
+            setEnabled(!getEnabled());
+            updateState();
+        }
+    }
+
+    private boolean getEnabled() {
+        return PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT)
+                .getBoolean(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE, false);
+    }
+
+    private void setEnabled(boolean enabled) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(Constants.CONTEXT);
+        sharedPrefs.edit().putBoolean(Constants.GLYPH_MUSIC_VISUALIZER_ENABLE, enabled).apply();
+        ServiceUtils.checkGlyphService();
+    }
+}

src/com/pixys/glyph/Tiles/TileActivity.java
@@ -39,7 +39,8 @@ public class TileActivity extends Activity {
         ComponentName sourceClass = getIntent().getParcelableExtra(Intent.EXTRA_COMPONENT_NAME);
         if (DEBUG) Log.d(TAG, "sourceClass: " + sourceClass.getClassName());
         if (sourceClass.getClassName().equals("com.pixys.glyph.Tiles.GlyphTileService")
-            || sourceClass.getClassName().equals("com.pixys.glyph.Tiles.TorchTileService")) {
+            || sourceClass.getClassName().equals("com.pixys.glyph.Tiles.TorchTileService")
+            || sourceClass.getClassName().equals("com.pixys.glyph.Tiles.MusicVisualizerTileService")) {
             openActivitySafely(new Intent(this, SettingsActivity.class));
         } else {
             finish();